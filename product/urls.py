be05adc380d36683a82a962ec50b1957e085c093
lenkamac <mackova.lenka@protonmail.com>
2025-11-04 10:16:54 +0100

    product detail

--- product/urls.py
+++ product/urls.py
@@ -6,4 +6,5 @@ app_name = 'product'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
-from django.db.models import Q
-from django.shortcuts import render
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from product.models import Product
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -13,3 +14,7 @@ class ProductListView(ListView):
     ordering = ['-id']
 
 
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'product/product-detail.html'
+    context_object_name = 'product'
