bdef17d3a6b5e36f6ca6b18a5bbb9c919075287e
lenkamac <mackova.lenka@protonmail.com>
2025-11-10 16:07:17 +0100

    add, delete and edit option for product in product list

--- product/urls.py
+++ product/urls.py
@@ -7,4 +7,7 @@ app_name = 'product'
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from product.models import Product
 from django.views.generic import ListView, DetailView
 
@@ -24,3 +25,56 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
         # Get all purchases for this product
         context['purchases'] = self.object.purchases.all().select_related('client', 'created_by')
         return context
+
+
+def add_product(request):
+    """Add a new product"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        net_price = request.POST.get('net_price')
+        sold_quantity = request.POST.get('sold_quantity', 0)
+        description = request.POST.get('description', '')
+
+        # Create new product
+        Product.objects.create(
+            name=name,
+            net_price=net_price,
+            sold_quantity=sold_quantity,
+            description=description
+        )
+
+        messages.success(request, 'Product added successfully!')
+        return redirect('product:product_list')
+
+    return render(request, 'product/add-product.html')
+
+
+def delete_product(request, product_id):
+    """Delete a product"""
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        product_name = product.name
+        product.delete()
+        messages.success(request, f'Product "{product_name}" has been deleted successfully!')
+        return redirect('product:product_list')
+
+    return render(request, 'product/delete-product.html', {'product': product})
+
+
+def edit_product(request, product_id):
+    """Edit an existing product"""
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        product.name = request.POST.get('name')
+        product.net_price = request.POST.get('net_price')
+        product.sold_quantity = request.POST.get('sold_quantity', 0)
+        product.description = request.POST.get('description', '')
+
+        product.save()
+
+        messages.success(request, f'Product "{product.name}" has been updated successfully!')
+        return redirect('product:product_list')
+
+    return render(request, 'product/edit-product.html', {'product': product})
